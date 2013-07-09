d0c991d5e331a7e356bfe30457dcd683d89e2916
jose pardo <oldlastman@gmail.com>
2013-07-09 23:02:36 +0000

    first pseudo functional grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var rest = require ('restler');
+var HTMLFILE_DEFAULT = "";//"index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT ="http://www.nasa.gov/multimedia/index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,16 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertUrlExists = function(inurl){
+    if(!inurl){
+	console.log("%s does not exist. Exiting.", inurl);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	}
+    return inurl;
+	
+}
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -54,21 +66,57 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
-
+var checkUrl = function(urlContentString,checksfile){
+  
+$ = cheerio.load(urlContentString);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    //return out;
+    showResult(out);
+}
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
+var showResult = function(checkJson){
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
+
 
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-u, --url <url>', 'website url',clone(assertUrlExists), URL_DEFAULT) 
+	.parse(process.argv);
+    
+    if (program.file){
+	    var checkJson = checkHtmlFile(program.file, program.checks);
+	   showResult(checkJson);
+	}else{
+	    console.log("antes url content");    
+	      rest.get(program.url).on('complete',function(result){
+	   
+		if (result instanceof Error) {
+		    console.log('Error: ' + result.message);
+   
+		} else {
+		   // console.log(result);
+		    var checkJson = checkUrl(result, program.checks);
+		}
+	    });	
+	
+		
+	}
+   // var outJson = JSON.stringify(checkJson, null, 4);
+   // console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
